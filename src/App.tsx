e3453f6d484483b09a4d9fe726b6a889181f6e47
rohitverma007 <rohitverma@live.ca>
2022-12-03 07:22:36 +0530

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,89 @@ import TabsBody from "./components/TabsBody";
 import { useSmartAccountContext } from "./contexts/SmartAccountContext";
 import { useWeb3AuthContext } from "./contexts/SocialLoginContext";
 import Button from "./components/Button";
+import { SingleEliminationBracket, Match, SVGViewer } from '@g-loot/react-tournament-brackets';
+// import { GlootTheme, StyledSvgViewer } from "styled-components";
+// JUST SEND JSON TO BACKEND.
+// THEN CREATE NFTS
+// DO FRONTEND LSAT
+
+const correct_matches = [
+  {
+    "netherlands_usa": "netherlands"
+  },
+  {
+    "argentina_australia": null
+  },
+  {
+    "japan_croatia": null
+  },
+  {
+    "brazil_south_korea": null
+  },
+  {
+    "france_poland": null
+  },
+  {
+    "england_senegal": null
+  },
+  {
+    "morocco_spain": null
+  },
+  {
+    "portugal_switzherland": null
+  }
+
+]
+for(const ob of correct_matches) {
+  for(const a in ob){
+    if(ob[a] != null){
+      if(ob[a] == user[a]){
+        return {
+          // NEW NFT
+        }
+      }
+    }
+  }
+}
+
+const matches = [
+    {
+      "netherlands_usa": "netherlands"
+    },
+    {
+      "argentina_australia": "argentina"
+    },
+    {
+      "japan_croatia": "japan"
+    },
+    {
+      "brazil_south_korea": "brazil"
+    },
+    {
+      "france_poland": "france"
+    },
+    {
+      "england_senegal": "england"
+    },
+    {
+      "morocco_spain": "spain"
+    },
+    {
+      "portugal_switzherland": "portugal"
+    }
+
+   ]
 
 const App: React.FC = () => {
   const classes = useStyles();
   const { connect, address, loading: eoaWalletLoading } = useWeb3AuthContext();
   const { loading } = useSmartAccountContext();
-
+  const onPartyClick = (party: typeof Match) => {
+    console.log(party)
+  }
+  const onMatchClick = (match: typeof Match) => {
+    console.log(match)
+  }
   if (!address) {
     return (
       <div
@@ -24,6 +101,110 @@ const App: React.FC = () => {
         }}
       >
         <h1 className={classes.title}>PredictNow</h1>
+        <SingleEliminationBracket
+
+      matches={[
+        {
+          "id": 1,
+          "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+          "tournamentRoundText": "4", // Text for Round Header
+          "startTime": "2021-05-30",
+          "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
+          "participants": [
+            {
+              "id": "netherlands", // Unique identifier of any kind
+              "resultText": "", // Any string works
+              "isWinner": false,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
+              "name": "Netherlands"
+            },
+            {
+              "id": "iusa",
+              "resultText": null,
+              "isWinner": false,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
+              "name": "USA"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+          "tournamentRoundText": "4", // Text for Round Header
+          "startTime": "2021-05-30",
+          "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
+          "participants": [
+            {
+              "id": "netherlands", // Unique identifier of any kind
+              "resultText": "", // Any string works
+              "isWinner": false,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
+              "name": "Netherlands"
+            },
+            {
+              "id": "iusa",
+              "resultText": null,
+              "isWinner": true,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
+              "name": "Ant"
+            }
+          ]          
+        }   
+      ]}
+      matchComponent={({
+        match,
+        onMatchClick,
+        onPartyClick,
+        onMouseEnter,
+        onMouseLeave,
+        topParty,
+        bottomParty,
+        topWon,
+        bottomWon,
+        topHovered,
+        bottomHovered,
+        topText,
+        bottomText,
+        connectorColor,
+        computedStyles,
+        teamNameFallback,
+        resultFallback,
+      }) => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-around',
+            color: '#000',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <div
+            onMouseEnter={() => {console.log(topParty);onMouseEnter(topParty.id);}}
+            style={{ display: 'flex' }}
+          >
+            <div>{topParty.name || teamNameFallback}</div>
+            <div>{topParty.resultText ?? resultFallback(topParty)}</div>
+          </div>
+          <div
+            style={{ height: '1px', width: '100%', background: '#FF8C00' }}
+          />
+          <div
+            onMouseEnter={() => onMouseEnter(bottomParty.id)}
+            style={{ display: 'flex' }}
+          >
+            <div>{bottomParty.name || teamNameFallback}</div>
+            <div>{bottomParty.resultText ?? resultFallback(topParty)}</div>
+          </div>
+        </div>
+      )}
+      svgWrapper={({ children, ...props }) => (
+        <SVGViewer width={500} height={500} {...props}>
+          {children}
+        </SVGViewer>
+      )}
+    />              
         <Button
           title="Get Started"
           onClickFunc={connect}
--- src/components/Onboarding/index.tsx
+++ src/components/Onboarding/index.tsx
@@ -114,19 +114,19 @@ const Onboarding: React.FC<OnboardingProps> = ({ setValue }) => {
 
   return (
     <main className={classes.main}>
- <SingleEliminationBracket
+<SingleEliminationBracket
       matches={[
         {
           "id": 260005,
           "name": "Fifa World Cup 2022",
-          "nextMatchId": 5, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+          "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
           "tournamentRoundText": "4", // Text for Round Header
           "startTime": "2021-05-30",
           "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
           "participants": [
             {
-              "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc", // Unique identifier of any kind
-              "resultText": "WON", // Any string works
+              "id": "netherlands", // Unique identifier of any kind
+              "resultText": "", // Any string works
               "isWinner": false,
               "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
               "name": "giacomo123"
@@ -171,7 +171,8 @@ const Onboarding: React.FC<OnboardingProps> = ({ setValue }) => {
           {children}
         </SVGViewer>
       )}
-    />    </main>
+    />        
+   </main>
   );
 };
 
