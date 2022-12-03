deca98515b7f7c7b7a470e0851c230bce143d320
rohitverma007 <rohitverma@live.ca>
2022-12-03 05:40:59 +0530

    add react brackets

--- src/components/Onboarding/index.tsx
+++ src/components/Onboarding/index.tsx
@@ -5,6 +5,8 @@ import { LocalRelayer, RestRelayer } from "@biconomy/relayer";
 import Button from "../Button";
 // import { useWeb3Context } from "../../contexts/Web3Context";
 import { useSmartAccountContext } from "../../contexts/SmartAccountContext";
+import { SingleEliminationBracket, DoubleEliminationBracket, Match, SVGViewer } from '@g-loot/react-tournament-brackets';
+
 import {
   getEOAWallet,
   showErrorMessage,
@@ -106,10 +108,46 @@ const Onboarding: React.FC<OnboardingProps> = ({ setValue }) => {
   function sleep(ms: any) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
+  const SingleElimination = () => (
+    <SingleEliminationBracket
+      matches={[
+        {
+          "id": 260005,
+          "name": "Final - Match",
+          "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
+          "tournamentRoundText": "4", // Text for Round Header
+          "startTime": "2021-05-30",
+          "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
+          "participants": [
+            {
+              "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc", // Unique identifier of any kind
+              "resultText": "WON", // Any string works
+              "isWinner": false,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
+              "name": "giacomo123"
+            },
+            {
+              "id": "9ea9ce1a-4794-4553-856c-9a3620c0531b",
+              "resultText": null,
+              "isWinner": true,
+              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
+              "name": "Ant"
+            }
+          ]
+        }
+      ]}
+      matchComponent={Match}
+      svgWrapper={({ children, ...props }) => (
+        <SVGViewer width={500} height={500} {...props}>
+          {children}
+        </SVGViewer>
+      )}
+    />
+  );
 
   return (
     <main className={classes.main}>
-     heu hey
+     SingleElimination()
     </main>
   );
 };
